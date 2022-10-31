a28222634a94e5c0111570a629d5b290edc44949
Jesper Lauridsen <jesperdlau@gmail.com>
2022-10-31 12:59:25 +0100

    Improved pipeline and visualization

--- train.py
+++ train.py
@@ -17,9 +17,9 @@ from utilities import scale_df
 # Hyperparameters 
 test_size= 0.25
 random_state= 42
-batch_size = 5
+batch_size = 10
 lr = 1e-4
-epochs = 10
+epochs = 3
 
 # Prepare data
 # Scale data here instead of preprocess as data may be used with other scalars in other parts of the project. 
@@ -50,11 +50,13 @@ optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
     model.train()
+    train_loss, train_correct = 0, 0
     for batch_nr, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.unsqueeze(1).to(device)
 
         # Compute prediction error
         pred = model(X)
+        pred_bin = torch.round(torch.sigmoid(pred))
         loss = loss_fn(pred, y)
 
         # Backpropagation
@@ -62,18 +64,23 @@ def train(dataloader, model, loss_fn, optimizer):
         loss.backward()
         optimizer.step()
 
+        # 
+        train_loss += loss.item()
+        train_correct += (pred_bin == y).type(torch.float).sum().item()
+
         # Print info for batch
-        if batch_nr % 100 == 0:
+        if batch_nr % 50 == 0:
             loss, current = loss.item(), batch_nr * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            print(f"train_loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+    return train_loss, train_correct
 
 
 # Test function
 def test(dataloader, model, loss_fn):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
+    #size = len(dataloader.dataset)
+    #num_batches = len(dataloader)
     model.eval()
-    test_loss, correct = 0, 0
+    test_loss, test_correct = 0, 0
     with torch.no_grad():
         for X, y in dataloader:
             X, y = X.to(device), y.unsqueeze(1).to(device)
@@ -82,27 +89,43 @@ def test(dataloader, model, loss_fn):
             pred_bin = torch.round(torch.sigmoid(pred))
 
             test_loss += loss_fn(pred, y).item()
-            correct += (pred_bin == y).type(torch.float).sum().item()
-    avg_loss = test_loss/num_batches
-    accuracy = correct/size
+            test_correct += (pred_bin == y).type(torch.float).sum().item()
+    #avg_loss = test_loss/num_batches
+    #accuracy = correct/size
     #print(f"Test Error: \n Accuracy: {(100*accuracy):>0.1f}%, Avg loss: {avg_loss:>8f} \n")
-    return accuracy, avg_loss
+    return test_loss, test_correct
 
 if __name__ == "__main__":
-    acc_list = []
-    loss_list = []
+    train_acc_list = []
+    test_acc_list = []
+    train_loss_list = []
+    test_loss_list = []
+
     for t in range(epochs):
+        print(f"-------------------------------\nEpoch {t+1}:")
+        train_loss, train_correct = train(dataloader_train, model, loss_fn, optimizer)
+        test_loss, test_correct = test(dataloader_test, model, loss_fn)
+
+        train_avg_loss = train_loss/len(dataloader_train)
+        train_accuracy = train_correct/len(dataloader_train.dataset)
+        test_avg_loss = test_loss/len(dataloader_test)
+        test_accuracy = test_correct/len(dataloader_test.dataset)
+
+        train_acc_list.append(train_accuracy)
+        train_loss_list.append(train_avg_loss)
+        test_acc_list.append(test_accuracy)
+        test_loss_list.append(test_avg_loss)
+
         
-        train(dataloader_train, model, loss_fn, optimizer)
-        accuracy, avg_loss = test(dataloader_test, model, loss_fn)
-
-        acc_list.append(accuracy)
-        loss_list.append(avg_loss)
-
-        print(f"Epoch {t+1}\n-------------------------------")
-        print(f"Test Error: \nAccuracy: {(100*accuracy):>0.1f}%, Avg loss: {avg_loss:>8f} \n")
+        #print(f"Test Error: \nAccuracy: {(100*accuracy):>0.1f}%, Avg loss: {avg_loss:>8f} \n")
 
     print("Done!")
-    plt.plot(acc_list)
+
+    #Plot 
+    plt.plot(train_acc_list)
+    plt.plot(test_acc_list)
+    plt.plot(train_loss_list, "--")
+    plt.plot(test_loss_list, "--")
     plt.ylim((0,1))
+    plt.legend(["train_acc", "test_acc", "train_loss", "test_loss"])
     plt.show()
