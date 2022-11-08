ec29f55fd64852a1decf819f53b54166d260be92
Jesper Lauridsen <jesperdlau@gmail.com>
2022-11-08 15:38:51 +0100

    Added regression model and changed class names

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 
-class NeuralNetwork(nn.Module):
+class ClassificationModel(nn.Module):
     def __init__(self):
         super().__init__()
         #self.flatten = nn.Flatten()
@@ -21,12 +21,29 @@ class NeuralNetwork(nn.Module):
         #x = x.squeeze()
         return x
 
+class RegressionModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(4, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1)
+        )
+
+    def forward(self, x):
+        x = self.linear_relu_stack(x)
+        return x
+
 
 if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Using {device} device")
 
-    model = NeuralNetwork().to(device)
+    model = ClassificationModel().to(device)
     print(model)
     print(model.parameters())
     
