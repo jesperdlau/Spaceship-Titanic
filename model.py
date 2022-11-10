be5b83d01946e0a02ccc069175d12e67c2413c6b
Jesper Lauridsen <jesperdlau@gmail.com>
2022-11-10 14:10:36 +0100

    Implemented the lambda variable to model

--- model.py
+++ model.py
@@ -25,23 +25,79 @@ class RegressionModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(13, 128),
+            nn.Linear(13, 32),
             nn.ReLU(),
-            nn.Linear(128, 128),
+            nn.Linear(32, 32),
             nn.ReLU(),
-            nn.Linear(128, 128),
-            nn.ReLU(),
-            nn.Linear(128, 128),
-            nn.ReLU(),
-            nn.Linear(128, 128),
-            nn.ReLU(),
-            nn.Linear(128, 1)
-        )
+            nn.Linear(32, 1))
 
     def forward(self, x):
         x = self.linear_relu_stack(x)
         return x
 
+class RegressionModelHyper1(nn.Module):
+    def __init__(self, hyper=0):
+        super().__init__()
+        if hyper == 1:
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(13, 32),
+                nn.ReLU(),
+                nn.Linear(32, 32),
+                nn.ReLU(),
+                nn.Linear(32, 32),
+                nn.ReLU(),
+                nn.Linear(32, 1))
+        elif hyper == 2:
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(13, 32),
+                nn.ReLU(),
+                nn.Linear(32, 32),
+                nn.ReLU(),
+                nn.Linear(32, 32),
+                nn.ReLU(),
+                nn.Linear(32, 32),
+                nn.ReLU(),
+                nn.Linear(32, 1))
+        else:
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(13, 32),
+                nn.ReLU(),
+                nn.Linear(32, 32),
+                nn.ReLU(),
+                nn.Linear(32, 1))
+
+    def forward(self, x):
+        x = self.linear_relu_stack(x)
+        return x
+
+class RegressionModelHyper2(nn.Module):
+    def __init__(self, hyper=0):
+        super().__init__()
+        if hyper == 1:
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(13, 64),
+                nn.ReLU(),
+                nn.Linear(64, 64),
+                nn.ReLU(),
+                nn.Linear(64, 1))
+        elif hyper == 2:
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(13, 128),
+                nn.ReLU(),
+                nn.Linear(128, 128),
+                nn.ReLU(),
+                nn.Linear(128, 1))
+        else:
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(13, 32),
+                nn.ReLU(),
+                nn.Linear(32, 32),
+                nn.ReLU(),
+                nn.Linear(32, 1))
+
+    def forward(self, x):
+        x = self.linear_relu_stack(x)
+        return x
 
 if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
